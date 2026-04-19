7f149cf548ab335361b9ae50e63930b6ee5bf56e
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-04-19 10:15:58 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,83 @@
+function capitalize(str) {
+  if (str.length === 0) {
+    return "";
+  }
+
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+
+  subtract(a, b) {
+    return a - b;
+  },
+
+  divide(a, b) {
+    return a / b;
+  },
+
+  multiply(a, b) {
+    return a * b;
+  },
+};
+
+function caesarCipher(str, shift) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const code = char.charCodeAt(0);
+
+    if (code >= 65 && code <= 90) {
+      const shifted = ((((code - 65 + shift) % 26) + 26) % 26) + 65;
+      result += String.fromCharCode(shifted);
+    } else if (code >= 97 && code <= 122) {
+      const shifted = ((((code - 97 + shift) % 26) + 26) % 26) + 97;
+      result += String.fromCharCode(shifted);
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+}
+
+function analyzeArray(arr) {
+  let sum = 0;
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return {
+    average: sum / arr.length,
+    min: min,
+    max: max,
+    length: arr.length,
+  };
+}
+
+export default {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
---
+++ main.test.js
@@ -0,0 +1,62 @@
+import main from "./main";
+
+test("capitalize first character", () => {
+  expect(main.capitalize("hello")).toBe("Hello");
+});
+
+test("capitalize keeps rest of string", () => {
+  expect(main.capitalize("pedro")).toBe("Pedro");
+});
+
+test("capitalize empty string", () => {
+  expect(main.capitalize("")).toBe("");
+});
+
+test("reverse a string", () => {
+  expect(main.reverseString("hello")).toBe("olleh");
+});
+
+test("reverse another string", () => {
+  expect(main.reverseString("Pedro")).toBe("ordeP");
+});
+
+test("calculator add", () => {
+  expect(main.calculator.add(2, 3)).toBe(5);
+});
+
+test("calculator subtract", () => {
+  expect(main.calculator.subtract(10, 4)).toBe(6);
+});
+
+test("calculator divide", () => {
+  expect(main.calculator.divide(12, 3)).toBe(4);
+});
+
+test("calculator multiply", () => {
+  expect(main.calculator.multiply(6, 7)).toBe(42);
+});
+
+test("caesarCipher basic shift", () => {
+  expect(main.caesarCipher("abc", 3)).toBe("def");
+});
+
+test("caesarCipher wraps from z to a", () => {
+  expect(main.caesarCipher("xyz", 3)).toBe("abc");
+});
+
+test("caesarCipher preserves case", () => {
+  expect(main.caesarCipher("HeLLo", 3)).toBe("KhOOr");
+});
+
+test("caesarCipher keeps punctuation and spaces", () => {
+  expect(main.caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+
+test("analyzeArray returns correct object", () => {
+  expect(main.analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
